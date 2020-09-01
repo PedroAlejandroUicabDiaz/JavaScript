cceebccd34bb9652acec76b78ccfca5b1b8dde27
PedroAlejandroUicabDiaz <1909180@upy.ed.mx>
2020-08-31 19:16:02 -0500

    FirstP

---
+++ JsBeginner.js
@@ -0,0 +1,42 @@
+// JavaScript basics
+// Basic math operations ------------
+
+//1+1;
+//90-70;
+//5-10;
+//10*-8;
+//1.1*1.1;
+//10*10;
+//10/5;
+//5.5/2.1;
+//-100/-5;
+//30%4;
+//20%2;
+
+//Math operations ------------------
+
+//(2+4)*10;
+//((5*(3+4*(5*9)))/2);
+//1/0;
+//-1/0;
+//0/0;
+
+//Some Comparations ---------------
+
+//1>5;
+//10<100;
+//2==2;
+//3!=5;
+//3!=3;
+
+//Strings -------------------------
+
+//'hello world';
+//"Hello World";
+//"I'm learning JavaScript";
+//"Hello" +" " + "World";
+//"1,2,"+3;
+//"a" < "b";
+//null == undefined;
+
+//Accessing to any characters -------
